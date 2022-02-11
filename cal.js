2fac550abf6cbd8b44947058ca53e25079c05c06
giaaaacomo <giacomoalbani@outlook.com>
2022-02-11 21:48:25 +0100

    .

---
+++ cal.js
@@ -0,0 +1,10 @@
+      document.addEventListener('DOMContentLoaded', function() {
+		  
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+         
+			googleCalendarApiKey: 'AIzaSyBqP-1vFO7wAUAoL1kdyf25psL6WOWsh-M',
+      		events: '59nv6e2sgh8ohets0c0vca0eac@group.calendar.google.com',
+        });
+        calendar.render();
+      });
