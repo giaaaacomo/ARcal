797bb3060d5300772842f5c5e8d26a89946cdb77
giaaaacomo <giacomoalbani@outlook.com>
2022-02-11 18:01:45 +0100

    .

---
+++ google_calendar.php
@@ -0,0 +1,11 @@
+<?php 
+
+$newcss = 'google_calendar.css';
+$f = file_get_contents('https://calendar.google.com/calendar/embed?src=[59nv6e2sgh8ohets0c0vca0eac@group.calendar.google.com]&ctz=Europe/Rome');
+$f = str_replace('//calendar.google.com','https://calendar.google.com',$f);
+$f = str_replace('/calendar/_','https://calendar.google.com/calendar/_',$f);
+$f = str_replace('<script>function _onload()', '<script>function _onload()', $f );
+
+echo $f;
+
+?>
